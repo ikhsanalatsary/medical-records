8f519bb7c568dfa98dbe0004d27fd0d3b867fd03
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-11-27 06:01:30 +0700

    feat(model:user): timestamp and pre_save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps::Created
+  include Mongoid::Timestamps::Updated
+
   field :first_name, type: String
   field :last_name, type: String
   field :display_name, type: String
   field :email, type: String
   field :username, type: String
   field :password_digest, type: String
+
+  before_create :pre_save
+
+  protected
+
+  def pre_save
+    self[:display_name] = "#{self[:first_name]} #{self[:last_name]}"
+  end
 end
