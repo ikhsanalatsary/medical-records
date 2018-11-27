c02fcdcdb27d2e096c263f3228047823aad010ee
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-11-27 07:30:55 +0700

    feat(model:user): basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   include Mongoid::Timestamps::Created
   include Mongoid::Timestamps::Updated
+  include ActiveModel::SecurePassword
 
   field :first_name, type: String
   field :last_name, type: String
@@ -9,6 +10,13 @@ class User
   field :email, type: String
   field :username, type: String
   field :password_digest, type: String
+
+  has_secure_password
+
+  validates_presence_of :first_name, message: 'Please fill in your first name'
+  validates_presence_of :last_name, message: 'Please fill in your last name'
+  validates_uniqueness_of :username, message: 'Username already exists'
+  validates_uniqueness_of :email, message: 'Please fill a valid email address', case_sensitive: false
 
   before_create :pre_save
 
