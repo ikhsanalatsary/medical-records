7c3a732aaf8cb2ba83915067e8ab1e2a1e355503
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2019-01-06 02:45:53 +0700

    crud patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,16 +1,50 @@
 class PatientsController < ApplicationController
+  before_action :set_patient_by_id, only: [
+    :show,
+    :update,
+    :destroy
+  ]
+
   def create
   end
 
   def index
+    @patients = Patient.all
+    json_response @patients
   end
 
   def show
+    json_response(@patient)
   end
 
   def update
+    @patient.update(patient_params)
+    head :no_content
   end
 
   def destroy
+    @patient.destroy
+    head :no_content
+  end
+
+  private
+
+  def patient_params
+    # whitelist params
+    params.permit(
+      :name,
+      :date_of_birth,
+      :sex,
+      :phone,
+      :blood_type,
+      { address: [] },
+      :person_in_charge,
+      :created_by,
+      :updated_by
+    )
+  end
+
+  def set_patient_by_id
+    @patient = Patient.find(params[:id])
   end
 end
